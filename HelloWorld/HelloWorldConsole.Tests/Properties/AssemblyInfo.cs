7005f5bc4fd277cbffbf19cc24f3d9e230fe59d0
rbteched <rbteched@outlook.com>
2017-08-22 18:14:43 -0500

    Add tests

---
+++ HelloWorld/HelloWorldConsole.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("HelloWorldConsole.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("HelloWorldConsole.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("64014f75-fe2c-4b29-aee5-1feae2171a5f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ HelloWorld/HelloWorldConsole.Tests/TextGeneratorTests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HelloWorldConsole.Tests
+{
+    [TestClass]
+    public class TextGeneratorTests
+    {
+        [TestMethod]
+        public void GenerateHelloWorldGeneratesExpectedText()
+        {
+            var textGenerator = new TextGenerator();
+            Assert.AreEqual("Hello World", textGenerator.GenerateHelloWorld());
+        }
+    }
+}
